f46f02fd12566be0b16eaa00ab4af7a3e6b66e58
Евгения Булкина <Evgenia15@users.noreply.github.com>
2020-05-15 14:08:58 +0400

    Добавьте файлы проекта.

---
+++ Tiles_2.2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tiles_2._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Pencil_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Foot_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Star_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Leaf_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Player_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Keyboard_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Cloud_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+    }
+}
